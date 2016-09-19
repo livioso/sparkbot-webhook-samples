7ee18054041c62131bf429edcd79829ab987a822
Livio Bieri <livio@livio.li>
2016-09-19 10:43:21 +0000

    Changed /remvoe to /done

--- examples/yobar.js
+++ examples/yobar.js
@@ -1,33 +1,46 @@
 /* 
- * a Cisco Spark bot that computes stats for a room
+ * a Cisco Spark bot that handles orders :)
+ * built at HackZurich 2016 :)
  * 
- * note : this example requires you set up a SPARK_TOKEN env variable for a real account (not a bot account), 
- *     as this code reads past messages in the room 
+ * note : this example requires you set up a SPARK_TOKEN env
+ *        variable for a real account (not a bot account), 
+ *        as this code reads past messages in the room 
  *  
  */
 
 var debug = require("debug")("samples");
 var fine = require("debug")("samples:fine");
 
-// Starts your Webhook with default configuration where the SPARK API access token is read from the SPARK_TOKEN env variable 
+// starts your Webhook with default configuration where
+// the SPARK API access token is read from the
+// SPARK_TOKEN env variable 
 var SparkBot = require("../sparkbot/webhook");
 var bot = new SparkBot();
 
 // do not listen to ourselves
-// comment the line below if you're running the bot from your Developer access token and you want to invoke in a test room
-//bot.interpreter.ignoreSelf = true; 
+// comment the line below if you're running the
+// bot from your Developer access token and
+// you want to invoke in a test room
+// bot.interpreter.ignoreSelf = true; 
 
 var SparkClient = require("node-sparky");
 var spark = new SparkClient({ token: process.env.SPARK_TOKEN });
 
 var todos = [];
-todos[111] = 'Yoghurt Sepp Blatter';
-todos[112] = 'Yoghurt Käsefüsse';
+
+bot.onCommand("reset", function (command) {
+    todos = [];
+});
 
 bot.onCommand("todo", function (command) {
-    spark.messageSendRoom(command.message.roomId, {
-        markdown: "#YoBot: We need to do the following yoghurts next:\n" + todos.join("\n")
-    });
+    var message = '#### YoBot: Nothing to do. Just chill.'
+    if(todos.length !== 0) {
+        message = '#### YoBot: Next Yoghurts:' + todos.join(', ');
+    }
+   
+   spark.messageSendRoom(command.message.roomId, {
+       markdown: message
+   });  
 });
 
 
@@ -36,18 +49,19 @@ bot.onCommand("add", function (command) {
   var args = command.args;
 
   spark.messageSendRoom(command.message.roomId, {
-      markdown: "_heard you ! adding yoghurt to the queue"
+      markdown: "> Adding Order with ID: " + todos.length
   });
 
-  todos[1] = 'lol';
+  todos.push(args.join(' '));
 });
 
-bot.onCommand("remove", function (command) {
+bot.onCommand("done", function (command) {
 
   var index = command.args[0];
 
   spark.messageSendRoom(command.message.roomId, {
-      markdown: "_heard you ! marking yoghurt as done"
+      markdown: "I heard you! marking yoghurt as done. :)",
+      files: "http://66.media.tumblr.com/632b2603278412e8381309d60eb1d078/tumblr_njr9nvsWeW1si3gq6o1_r1_500.gif",
   });
 
   todos[index] = undefined;
